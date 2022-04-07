dfe133e5ef775ec5d67e057266630fe17119c95f
rudzupuke <diana.gavare2@gmail.com>
2022-04-07 15:08:07 +0300

    add search input

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 "use strict";
 
 // HELPER FUNCTIONS //
+
+///// CREATING SEARCH INPUT FIELD AND BUTTON /////
 const createSearchInputField = () => {
     const searchInput = document.createElement("input");
+    searchInput.classList.add("search-input");
     searchInput.setAttribute("type", "text");
-    searchInput.setAttribute("placeholder", "Spell...");
+    searchInput.setAttribute("id", "search-input");
+    searchInput.setAttribute("placeholder", "Search spell..");
 
     return searchInput;
 };
 
-const createSearchButton = () => {
-    const searchButton = document.createElement("button");
-    searchButton.innerText = "Search Spell";
+const createSearchClearButton = () => {
+    const clearButton = document.createElement("button");
+    clearButton.classList.add("button");
+    clearButton.innerText = "Clear";
 
-    return searchButton;
+    return clearButton;
 };
 
-const createAllSpellContainer = () => {
+///// CREATING SPELL CONTAINERS /////
+
+const getAllSpellContainer = () => {
     const spellsElement = document.getElementById("spells");
     spellsElement.innerHTML = "";
 
     return spellsElement;
 };
 
-const createSpellContainer = (spellsName, spellsDesc) => {
+const createOneSpellContainer = (spellsName, spellsDesc) => {
     const spellContainer = document.createElement("div");
     spellContainer.className = "spell__container";
     spellContainer.append(spellsName);
@@ -48,9 +55,11 @@ const createSpellsDesc = (description) => {
     return spellsDesc;
 };
 
-const toggleOneSpellsDesc = (spellsDesc, spellsName) => {
-    spellsName.classList.toggle("wand");
+/////
+
+const toggleSpell = (spellsDesc, spellsName) => {
     spellsName.classList.toggle("wand-bottom");
+    spellsName.classList.toggle("wand");
     spellsDesc.classList.toggle("hidden");
 };
 
@@ -58,6 +67,12 @@ const closeSpell = (spellName, spellDesc) => {
     spellDesc.classList.add("hidden");
     spellName.classList.remove("wand-bottom");
     spellName.classList.add("wand");
+};
+
+const showSpell = (spellName, spellDesc) => {
+    spellDesc.classList.remove("hidden");
+    spellName.classList.add("wand-bottom");
+    spellName.classList.remove("wand");
 };
 
 const selectSpellNameAndDesc = (spellContainer) => {
@@ -87,9 +102,7 @@ const showAllSpells = () => {
     allSpells.forEach((spellContainer) => {
         const { spellName, spellDesc } = selectSpellNameAndDesc(spellContainer);
 
-        spellDesc.classList.remove("hidden");
-        spellName.classList.add("wand-bottom");
-        spellName.classList.add("wand");
+        showSpell(spellName, spellDesc);
     });
 };
 const closeAllSpells = () => {
@@ -102,61 +115,122 @@ const closeAllSpells = () => {
     });
 };
 
+///// SEARCH INPUT ////
+
+const searchInput = createSearchInputField();
+document.getElementById("search-container").appendChild(searchInput);
+const clearButton = createSearchClearButton();
+document.getElementById("search-container").appendChild(clearButton);
+
+const showSearchedSpells = (e) => {
+    const allSpells = document.querySelectorAll("#spells .spell__container");
+
+    allSpells.forEach((spellContainer) => {
+        const { spellName, spellDesc } = selectSpellNameAndDesc(spellContainer);
+
+        if (
+            e.target.value.length > 0 &&
+            spellName.innerText
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase())
+        ) {
+            showSpell(spellName, spellDesc);
+        } else if (e.target.value.length === 0) {
+            spellContainer.classList.remove("hidden");
+            closeSpell(spellName, spellDesc);
+        } else {
+            closeSpell(spellName, spellDesc);
+            spellContainer.classList.add("hidden");
+        }
+    });
+};
+
+const handleClickOnClear = () => {
+    const allSpells = document.querySelectorAll("#spells .spell__container");
+    searchInput.value = "";
+
+    allSpells.forEach((spellContainer) => {
+        spellContainer.classList.remove("hidden");
+    });
+    closeAllSpells();
+};
+
 const spellList = () => {
     const spellData = [
         {
-            spell: "Expecto Patronum (Patronus Charm)",
-            description:
-                "This ancient and mysterious charm conjures a magical guardian, a projection of all your most positive feelings.",
+            en: {
+                spell: "Expecto Patronum (Patronus Charm)",
+                description:
+                    "This ancient and mysterious charm conjures a magical guardian, a projection of all your most positive feelings.",
+            },
         },
         {
-            spell: "Confundo (Confundus charm)",
-            description:
-                "The Confundus Charm (Confundo) is a charm which confuses and misdirects the target. It works on living and inanimate things, as long as the target has a mind to be confused.",
+            en: {
+                spell: "Confundo (Confundus charm)",
+                description:
+                    "The Confundus Charm (Confundo) is a charm which confuses and misdirects the target. It works on living and inanimate things, as long as the target has a mind to be confused.",
+            },
         },
         {
-            spell: "Obliviate (Memory charm)",
-            description:
-                "The Memory Charm, also known as the Forgetfulness Charm, is a charm that can be used to erase specific memories from an individual's mind.",
+            en: {
+                spell: "Obliviate (Memory charm)",
+                description:
+                    "The Memory Charm, also known as the Forgetfulness Charm, is a charm that can be used to erase specific memories from an individual's mind.",
+            },
         },
         {
-            spell: "Reparo (Mending charm)",
-            description:
-                "The Mending Charm, also known as the Repairing Charm (Reparo), is a charm that can be used to seamlessly repair a broken object and works on most materials.",
+            en: {
+                spell: "Reparo (Mending charm)",
+                description:
+                    "The Mending Charm, also known as the Repairing Charm (Reparo), is a charm that can be used to seamlessly repair a broken object and works on most materials.",
+            },
         },
         {
-            spell: "Riddikulus (Boggart-Banishing spell)",
-            description:
-                "The Boggart-Banishing Spell (Riddikulus) is a charm that is used to defeat a Boggart. It causes the creature to assume a form that is humorous to the caster, along with a whip-crack noise, thereby taking away the Boggart's ability to terrorise.",
+            en: {
+                spell: "Riddikulus (Boggart-Banishing spell)",
+                description:
+                    "The Boggart-Banishing Spell (Riddikulus) is a charm that is used to defeat a Boggart. It causes the creature to assume a form that is humorous to the caster, along with a whip-crack noise, thereby taking away the Boggart's ability to terrorise.",
+            },
         },
         {
-            spell: "Accio (Summoning charm)",
-            description:
-                "The Summoning Charm (Accio) is a charm that summons an object toward the caster. It is able to summon objects in direct line of sight of the caster, as well as things out of view, by calling the object aloud after the incantation",
+            en: {
+                spell: "Accio (Summoning charm)",
+                description:
+                    "The Summoning Charm (Accio) is a charm that summons an object toward the caster. It is able to summon objects in direct line of sight of the caster, as well as things out of view, by calling the object aloud after the incantation",
+            },
         },
         {
-            spell: "Alohomora (Unlocking charm)",
-            description:
-                "The Unlocking Charm (Alohomora), also known as the Thief's Friend, is a charm that unlocks objects such as doors or windows.",
+            en: {
+                spell: "Alohomora (Unlocking charm)",
+                description:
+                    "The Unlocking Charm (Alohomora), also known as the Thief's Friend, is a charm that unlocks objects such as doors or windows.",
+            },
         },
         {
-            spell: "Petrificus Totalus (Full body-bind curse)",
-            description:
-                "The Full Body-Bind Curse (Petrificus Totalus), also known as the Body Freezing Spell, is a curse that temporarily paralyses the opponent.",
+            en: {
+                spell: "Petrificus Totalus (Full body-bind curse)",
+                description:
+                    "The Full Body-Bind Curse (Petrificus Totalus), also known as the Body Freezing Spell, is a curse that temporarily paralyses the opponent.",
+            },
         },
         {
-            spell: "Wingardium Leviosa (Levitation charm)",
-            description:
-                "The Levitation Charm (Wingardium Leviosa) is a charm to make objects fly, or levitate.",
+            en: {
+                spell: "Wingardium Leviosa (Levitation charm)",
+                description:
+                    "The Levitation Charm (Wingardium Leviosa) is a charm to make objects fly, or levitate.",
+            },
         },
         {
-            spell: "Lumos (Wand-lighting charm)",
-            description:
-                "The Wand-Lighting Charm (Lumos) is a charm that illuminates the tip of the caster's wand, allowing the caster to see in the dark.",
+            en: {
+                spell: "Lumos (Wand-lighting charm)",
+                description:
+                    "The Wand-Lighting Charm (Lumos) is a charm that illuminates the tip of the caster's wand, allowing the caster to see in the dark.",
+            },
         },
         {
-            spell: "Unforgivable curses",
-            description: ` The Unforgivable Curses are three of the most powerful and sinister
+            en: {
+                spell: "Unforgivable curses",
+                description: ` The Unforgivable Curses are three of the most powerful and sinister
               spells known to the wizarding world:
               <ul>
                 <li>
@@ -191,31 +265,29 @@ const spellList = () => {
                   </ul>
                 </li>
               </ul>`,
+            },
         },
         {
-            spell: "Want to learn more spells?",
-            description: `You can do by clicking here 👉
+            en: {
+                spell: "Want to learn more spells?",
+                description: `You can do by clicking here 👉
               <a class="button"
                 href="https://harrypotter.fandom.com/wiki/List_of_spells"
                 target="_blank"
                 >Learn More</a
               >`,
+            },
         },
     ];
 
-    const searchInput = createSearchInputField();
-    document.getElementById("search-container").appendChild(searchInput);
-    const searchButton = createSearchButton();
-    document.getElementById("search-container").appendChild(searchButton);
-
-    const allSpellsContainer = createAllSpellContainer();
+    const allSpellsContainer = getAllSpellContainer();
     const handleClickOnSpell = (spellsName, spellsDesc) => {
         closeOtherSpellsDesc(spellsDesc);
-        toggleOneSpellsDesc(spellsDesc, spellsName);
+        toggleSpell(spellsDesc, spellsName);
     };
 
     spellData.forEach((spellEntry) => {
-        const { spell, description } = spellEntry;
+        const { spell, description } = spellEntry.en;
 
         const spellsName = createSpellsName(spell);
         const spellsDesc = createSpellsDesc(description);
@@ -232,33 +304,14 @@ const spellList = () => {
             closeAllSpells();
         };
 
-        const spellContainer = createSpellContainer(spellsName, spellsDesc);
+        const spellContainer = createOneSpellContainer(spellsName, spellsDesc);
 
         allSpellsContainer.append(spellContainer);
     });
 
-    searchInput.oninput = (e) => {
-        const allSpells = document.querySelectorAll(
-            "#spells .spell__container"
-        );
-        console.log(e.target.value);
+    searchInput.oninput = (e) => showSearchedSpells(e);
 
-        allSpells.forEach((spellContainer) => {
-            const { spellName, spellDesc } =
-                selectSpellNameAndDesc(spellContainer);
-            spellName.classList.add("hidden");
-            spellDesc.classList.add("hidden");
-            if (
-                spellName.innerText.toLowerCase().includes(e.target.value) ||
-                spellName.innerText.includes(e.target.value)
-            ) {
-                spellDesc.classList.remove("hidden");
-                spellName.classList.remove("hidden");
-                spellName.classList.remove("wand");
-                spellName.classList.add("wand-bottom");
-            }
-        });
-    };
+    clearButton.onclick = () => handleClickOnClear();
 };
 
 spellList();
